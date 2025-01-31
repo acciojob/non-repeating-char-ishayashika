c054f09fb6f8033ddf88ef86c40c1c91125a7a44
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-01-31 22:46:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const count=[];
+	//will count the frequency of charchter present in string
+	for(let i=0;i<str.length;i++){
+		count[str[i]]=(count[str[i]] || 0)++;
+	}
+	for(let i=0;i<str.length;i++){
+		if(count[str[i]]===1){
+			return str[i];
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
+
